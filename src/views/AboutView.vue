61b4672a8bae5f33df54f11e222dfc998156bcae
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2023-12-20 14:53:52 -0300

    Ajustes  no index e views

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
+<!-- aqui que vai vir as coisas do crud?-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Usuários </h1>
+
+    <div class="row">
+      <div class="col-3">
+        Nome
+      </div>
+      <div class="col-3">
+        Email
+      </div>
+      <div class="col-3">
+        Cpf
+      </div>
+      <div class="col-3">
+        endereço
+      </div>
+      <div class="col-3">
+        departamento
+      </div>
+    </div>
   </div>
 </template>
